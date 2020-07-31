b70df77c6be2161e2bef4e575e6a8c97331a9e0e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-07-31 22:52:02 +0530

    [DB] seed default roles into db using migration

---
+++ server/migrations/1596215396877-SeedRoles.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner, getRepository} from 'typeorm';
+
+import {RolesSeed} from '../seeds/roles.seed';
+import {Role} from '../models/Role';
+
+export class SeedRoles1596215396877 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Seed roles into table
+    await getRepository(Role).save(RolesSeed);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // do nothing
+  }
+}
---
+++ server/seeds/roles.seed.ts
@@ -0,0 +1,22 @@
+export const RolesSeed = [
+  {
+    name: 'admin',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'buyer',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'seller',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'agent',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
