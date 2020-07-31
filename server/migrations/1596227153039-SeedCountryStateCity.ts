5a45e1cf56217bc2e3c035bab89629a0a447f86b
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-08-01 02:03:53 +0530

    [DB] seed some country, state & cities into db with migration scripts

---
+++ server/migrations/1596227153039-SeedCountryStateCity.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner, getRepository} from 'typeorm';
+
+import {CitySeed, CountrySeed, StateSeed} from '../seeds/location.seed';
+import {Country} from '../models/Country';
+import {State} from '../models/State';
+import {City} from '../models/City';
+
+export class SeedCountryStateCity1596227153039 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Seed locations into table
+    await getRepository(City).save(CitySeed);
+    await getRepository(State).save(StateSeed);
+    await getRepository(Country).save(CountrySeed);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
---
+++ server/seeds/location.seed.ts
@@ -0,0 +1,70 @@
+export const CountrySeed = [
+  {
+    code: '+91',
+    name: 'India',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    code: '+1',
+    name: 'United States',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    code: '+44',
+    name: 'United Kingdom',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+export const StateSeed = [
+  {
+    code: 'KL',
+    name: 'Kerala',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    code: 'DL',
+    name: 'Delhi',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    code: 'GA',
+    name: 'Goa',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    code: 'PB',
+    name: 'Punjab',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+export const CitySeed = [
+  {
+    name: 'Thiruvananthapuram',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Pathanamthitta',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Kochi',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Kollam',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
