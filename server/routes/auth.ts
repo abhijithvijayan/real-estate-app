06ff5eeb606ed242190744289f6e6b4e212e1355
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-07-31 18:14:27 +0530

    [AUTH] endpoint for user signin

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,10 +1,14 @@
 import asyncHandler from 'express-async-handler';
 import {Router} from 'express';
 
+import * as AuthController from '../controllers/AuthController';
 import UserController from '../controllers/UserController';
 
 const router = Router();
 
 router.post('/signup', asyncHandler(UserController.signup));
+router.post('/signin', asyncHandler(AuthController.localAuth), (req, res) =>
+  res.send({status: true})
+);
 
 export default router;
