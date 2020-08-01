dedcfdc43e2d3bad062f28110af5fd8d3cc5edc1
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-08-02 01:09:53 +0530

    [SSR] use swr with axios for SSR data fetching & caching

---
+++ client/api/useRequest.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types,  @typescript-eslint/explicit-function-return-type */
+import useSWR, {ConfigInterface} from 'swr';
+import api, {ApiRequestProps} from './index';
+
+/**
+ *  ApiRequestProps => fireUpApiRequest() config
+ *  ConfigInterface => swr config
+ */
+export default function useRequest(
+  {key, route = [], params = {}}: ApiRequestProps,
+  {initialData, ...config}: ConfigInterface = {}
+) {
+  const {data: response, error, isValidating, mutate} = useSWR(
+    (): any => {
+      return api({key, route, params});
+    },
+    {
+      ...config,
+      initialData: initialData && {
+        status: 200,
+        statusText: 'InitialData',
+        headers: {},
+        data: initialData,
+      },
+    }
+  );
+
+  return {
+    data: response && response.data,
+    response,
+    error,
+    isValidating,
+    mutate,
+  };
+}
